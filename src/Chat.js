f1ffc3b62c798fcc920384e2e833d996c1dbb3fd
fangz2 <fangz2@rose-hulman.edu>
2018-06-12 16:17:11 -0400

    style chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import ChatHeader from './ChatHeader';
-import MessageList from './MessageList';
-import MessageForm from './MessageForm';
+import ChatHeader from './ChatHeader'
+import MessageList from './MessageList'
+import MessageForm from './MessageForm'
 
 
 
@@ -30,7 +30,9 @@ class Chat extends Component {
 
     render() {
       return (
-        <div className='Chat' >
+        <div 
+            className='Chat'
+            style={styles} >
             <ChatHeader/>
             <MessageList messages={this.state.messages}/>
             <MessageForm addMessage={this.addMessage} />
@@ -38,6 +40,12 @@ class Chat extends Component {
         </div>   
       )
     }
+}
+
+const styles={
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
 }
 
 export default Chat
